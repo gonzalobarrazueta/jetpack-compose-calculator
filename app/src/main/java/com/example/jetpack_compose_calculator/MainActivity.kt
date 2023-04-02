fb4b1d27d3b8ec48ba9ab3e489ebfb1525202a3c
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-02 14:26:00 -0500

    Add MathOperator class and logic to display it

--- app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.jetpack_compose_calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,22 +30,24 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Calculator()
+
                 }
             }
         }
     }
 }
 
-@Preview
 @Composable
 fun Calculator() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        NumberTextField();
+        NumberTextField()
         Spacer(modifier = Modifier.height(30.dp))
-        NumberTextField();
+        ShowMathOperator(operator = MathOperator(Icons.Outlined.Add, "Plus", {}))
+        Spacer(modifier = Modifier.height(30.dp))
+        NumberTextField()
     }
 }
 
@@ -61,8 +68,23 @@ fun NumberTextField() {
     )
 }
 
-//@Preview
+data class MathOperator(val icon: ImageVector, val description: String, val onClick: () -> Unit)
+
 @Composable
-fun PreviewTextField() {
-    NumberTextField()
+fun ShowMathOperator(operator: MathOperator) {
+    IconButton(
+        onClick = { operator.onClick() },
+        modifier = Modifier
+            .size(48.dp)
+            .padding(all = 10.dp)
+            .border(1.5.dp, MaterialTheme.colors.primary, CircleShape),
+    ) {
+        Icon(operator.icon, operator.description)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewComponent() {
+    ShowMathOperator(operator = MathOperator(Icons.Outlined.Add, "Plus", {}))
 }
