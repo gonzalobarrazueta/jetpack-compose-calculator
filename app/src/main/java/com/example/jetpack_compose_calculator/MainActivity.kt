b95a95bbdcf222032e7057cbc9f2b0b7febf633c
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-02 15:46:41 -0500

    Add list of operators

--- app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -13,7 +15,6 @@ import androidx.compose.material.icons.outlined.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +31,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Calculator()
-
                 }
             }
         }
@@ -45,9 +45,10 @@ fun Calculator() {
     ) {
         NumberTextField()
         Spacer(modifier = Modifier.height(30.dp))
-        ShowMathOperator(operator = MathOperator(Icons.Outlined.Add, "Plus", {}))
+        ListOfOperators()
         Spacer(modifier = Modifier.height(30.dp))
         NumberTextField()
+        Text(text = "The operator selected is $operatorType")
     }
 }
 
@@ -68,7 +69,14 @@ fun NumberTextField() {
     )
 }
 
-data class MathOperator(val icon: ImageVector, val description: String, val onClick: () -> Unit)
+@Composable
+fun ListOfOperators() {
+    LazyRow {
+        items(operatorsFunctionality) { operator ->
+            ShowMathOperator(operator)
+        }
+    }
+}
 
 @Composable
 fun ShowMathOperator(operator: MathOperator) {
