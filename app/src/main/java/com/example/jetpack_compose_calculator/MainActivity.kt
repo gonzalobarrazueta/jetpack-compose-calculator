86b16984dc740ea57cce41febe1e25a88ca24290
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-01 22:02:58 -0500

    Add text field

--- app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.jetpack_compose_calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_calculator.ui.theme.JetpackcomposecalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +32,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun textField() {
+
+    var firstNumber by remember { mutableStateOf("Add a number") }
+
+    OutlinedTextField(
+        value = firstNumber,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.border(1.dp, MaterialTheme.colors.primary),
+        onValueChange = { firstNumber = it },
+    )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetpackcomposecalculatorTheme {
-        Greeting("Android")
-    }
+fun PreviewTextField() {
+    textField();
 }
