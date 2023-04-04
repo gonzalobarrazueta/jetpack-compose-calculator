9dbba6325ef9fcfb39b01df5e4a2f57e281612d2
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-03 23:59:11 -0500

    Add App bar

--- app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_calculator/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.icons.outlined.Calculate
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,6 +46,7 @@ fun Calculator() {
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        AppBar()
         NumberTextField()
         Spacer(modifier = Modifier.height(30.dp))
         ListOfOperators()
@@ -53,6 +55,15 @@ fun Calculator() {
         Spacer(modifier = Modifier.height(30.dp))
         CalculateButton()
     }
+}
+
+@Composable
+fun AppBar() {
+    TopAppBar(
+        title = { Text(text = "Calculator")},
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = Color.White
+    )
 }
 
 @Composable
